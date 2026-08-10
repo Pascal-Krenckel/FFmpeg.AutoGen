d73938aa897f966ee64fedf5bd5f0623d66ca39f
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-08-11 01:58:32 +0200

    FixedArray is now Pinnable and have ref indexer. Added IFixedPtrArray

--- FFmpeg.AutoGen.Abstractions/IFixedArray.cs
+++ FFmpeg.AutoGen.Abstractions/IFixedArray.cs
@@ -5,9 +5,36 @@ public interface IFixedArray
     int Length { get; }
 }
 
-internal interface IFixedArray<T> : IFixedArray
+public interface IFixedArray<T> : IFixedArray
 {
-    T this[uint index] { get; set; }
+    ref T this[int index] { get; }
+    ref T this[uint index] { get; }
+
     T[] ToArray();
     void UpdateFrom(T[] array);
+    ref T GetPinnableReference();
+
+    //public Span<T> AsSpan() => MemoryMarshal.CreateSpan(ref GetPinnableReference(), Length); 
+}
+
+public unsafe interface IFixedPtrArray : IFixedArray
+{
+    void* this[int index] { get; set; }
+    void* this[uint index] { get; set; }
+
+    void*[] ToArray();
+    void UpdateFrom(void*[] array);
+
+    ref void* GetPinnableReference();
+}
+
+public unsafe interface IFixedPtrArray<T> : IFixedPtrArray where T : unmanaged
+{
+    T* this[int index] { get; set; }
+    T* this[uint index] { get; set; }
+
+    T*[] ToArray();
+    void UpdateFrom(T*[] array);
+
+    ref T* GetPinnableReference();
 }
