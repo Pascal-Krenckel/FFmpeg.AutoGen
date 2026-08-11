d4b7a3ea0fab396828d454da63f9f8b79f92510c
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-08-11 02:10:39 +0200

    FixedArrys now pinnable. IFixedPtrArray added.

--- FFmpeg.AutoGen.CppSharpUnsafeGenerator/Generation/FixedArraysGenerator.cs
+++ FFmpeg.AutoGen.CppSharpUnsafeGenerator/Generation/FixedArraysGenerator.cs
@@ -19,6 +19,9 @@ internal sealed class FixedArraysGenerator : GeneratorBase<FixedArrayDefinition>
     public override IEnumerable<string> Usings()
     {
         yield return "System";
+        yield return "System.ComponentModel";
+        yield return "System.Runtime.CompilerServices";
+        yield return "System.Runtime.InteropServices";
     }
 
     protected override IEnumerable<FixedArrayDefinition> Query(IEnumerable<FixedArrayDefinition> arrays) =>
@@ -30,70 +33,219 @@ internal sealed class FixedArraysGenerator : GeneratorBase<FixedArrayDefinition>
         int length = array.Length;
         var elementType = array.ElementType;
         string elementTypeName = ParametersHelper.GetTypeName(elementType, useLegacy);
+        string nonPtrElementTypeName = elementTypeName.Replace("*", "");
         string arrayName = useLegacy ? array.LegacyName : array.Name;
-
-        WriteLine(array.IsPointer ? $"public unsafe struct {arrayName} : IFixedArray" : $"public unsafe struct {arrayName} : IFixedArray<{elementTypeName}>");
+        string interfaceName = array.IsPointer ? nonPtrElementTypeName == "void" ?
+            "IFixedPtrArray" :
+            $"IFixedPtrArray<{nonPtrElementTypeName}>" :
+            $"IFixedArray<{elementTypeName}>";
+        WriteLine("[StructLayout(LayoutKind.Sequential)]");
+        WriteLine($"public unsafe struct {arrayName} : {interfaceName}");
 
         using (BeginBlock())
         {
             string lengthPropertyName = useLegacy ? "Size" : "ArrayLength";
             WriteLine($"public static readonly int {lengthPropertyName} = {length};");
-            WriteLine($"public int Length => {length};");
+            WriteLine($"public readonly int Length => {length};");
 
             if (array.IsPrimitive)
                 WritePrimitiveFixedArray(elementTypeName, length);
+            else if (array.IsPointer)
+            {
+                if (nonPtrElementTypeName == "void")
+                    WriteVoidPointerType(length, false);
+                else
+                    WritePointerType(elementTypeName, length);
+
+            }
             else
                 WriteComplexFixedArray(elementTypeName, length);
 
-            WriteLine($"public static implicit operator {elementTypeName}[]({arrayName} @struct) => @struct.ToArray();");
+            if (!array.IsPointer)
+            {               
+                WriteLine($"public static implicit operator Span<{elementTypeName}>({arrayName} array) => array.AsSpan();");
+                WriteLine($"public static implicit operator ReadOnlySpan<{elementTypeName}>({arrayName} array) => array.AsReadOnlySpan();");
+            }
+            else
+            {
+                WriteLine($"public static implicit operator Span<IntPtr>({arrayName} array) => array.AsSpan();");
+                WriteLine($"public static implicit operator ReadOnlySpan<IntPtr>({arrayName} array) => array.AsReadOnlySpan();");
+            }
+
+            WriteLine("private static void CheckIndex(int index)");
+            using (BeginBlock())
+            {
+                WriteLine($"if ((uint)index >= {length}) throw new ArgumentOutOfRangeException();");
+            }
+
+            WriteLine("private static void CheckIndex(uint index)");
+            using (BeginBlock())
+            {
+                WriteLine($"if (index >= {length}) throw new ArgumentOutOfRangeException();");
+            }
         }
 
         WriteLine();
     }
-
 
     private void WritePrimitiveFixedArray(string elementType, int length)
     {
         WriteLine($"fixed {elementType} _[{length}];");
         WriteLine();
 
-        WriteLine($"public {elementType} this[uint i]");
+        WriteLine($"public ref {elementType} this[int i] => ref _[i];");
+        WriteLine($"public ref {elementType} this[uint i] => ref _[(int)i];");
 
-        using (BeginBlock())
-        {
-            WriteLine("get => _[i];");
-            WriteLine("set => _[i] = value;");
-        }
 
         WriteLine($"public {elementType}[] ToArray()");
         using (BeginBlock())
-            WriteLine($"var a = new {elementType}[{length}]; for (uint i = 0; i < {length}; i++) a[i] = _[i]; return a;");
+            WriteLine($"var a = new {elementType}[{length}]; for (int i = 0; i < {length}; i++) a[i] = _[i]; return a;");
 
         WriteLine($"public void UpdateFrom({elementType}[] array)");
         using (BeginBlock())
-            WriteLine($"uint i = 0; foreach(var value in array) {{ _[i++] = value; if (i >= {length}) return; }}");
+        {
+            WriteLine($"int length = Math.Min(array.Length, {length});");
+            WriteLine($"for(int i = 0; i < length; i++) _[i] = array[i];");
+        }
+        WriteLine("[EditorBrowsable(EditorBrowsableState.Never)]");
+        WriteLine($"public ref {elementType} GetPinnableReference() => ref _[0];");
+
+        WriteLine($"public Span<{elementType}> AsSpan() {{ fixed(void* ptr = _) return new Span<{elementType}>(ptr, {length}); }}");
+        WriteLine($"public ReadOnlySpan<{elementType}> AsReadOnlySpan() {{ fixed(void* ptr = _) return new ReadOnlySpan<{elementType}>(ptr, {length}); }}");
     }
 
     private void WriteComplexFixedArray(string elementType, int length)
     {
-        WriteLine(string.Join(" ", Enumerable.Range(0, length).Select(i => $"{elementType} _{i};")));
+        WriteLine("#pragma warning disable CS9084");
+
+        WriteLine($"{elementType} " + string.Join(",", Enumerable.Range(0, length).Select(i => $"_{i}")) + ";");
         WriteLine();
 
-        string @fixed = $"fixed ({elementType}* p0 = &_0)";
-        WriteLine($"public {elementType} this[uint i]");
-
+        WriteLine($"public ref {elementType} this[int i] ");
         using (BeginBlock())
         {
-            WriteLine($"get {{ if (i >= {length}) throw new ArgumentOutOfRangeException(); {@fixed} {{ return *(p0 + i); }} }}");
-            WriteLine($"set {{ if (i >= {length}) throw new ArgumentOutOfRangeException(); {@fixed} {{ *(p0 + i) = value;  }} }}");
+            WriteLine($"get {{ CheckIndex(i); return ref Unsafe.Add(ref _0, i); }}");
+        }
+
+        WriteLine($"public ref {elementType} this[uint i] ");
+        using (BeginBlock())
+        {
+            WriteLine($"get {{  CheckIndex(i); return ref Unsafe.Add(ref _0, i); }}");
         }
 
         WriteLine($"public {elementType}[] ToArray()");
         using (BeginBlock())
-            WriteLine($"{@fixed} {{ var a = new {elementType}[{length}]; for (uint i = 0; i < {length}; i++) a[i] = *(p0 + i); return a; }}");
+        {
+            WriteLine($"var a = new {elementType}[{length}];");
+            WriteLine($"for (int i = 0; i < {length}; i++) a[i] = this[i];");
+            WriteLine($"return a;");
+        }
 
         WriteLine($"public void UpdateFrom({elementType}[] array)");
         using (BeginBlock())
-            WriteLine($"{@fixed} {{ uint i = 0; foreach(var value in array) {{ *(p0 + i++) = value; if (i >= {length}) return; }} }}");
+        {
+            WriteLine($"int length = Math.Min(array.Length, Length);");
+            WriteLine($"for(int i = 0; i < length; i++) this[i] = array[i];");
+        }
+        
+        WriteLine("[EditorBrowsable(EditorBrowsableState.Never)]");
+        WriteLine($"public ref {elementType} GetPinnableReference() => ref _0;");
+
+
+        WriteLine("#pragma warning restore CS9084");
+
+        WriteLine($"public Span<{elementType}> AsSpan() {{ fixed(void* ptr = &_0) return new Span<{elementType}>(ptr, {length}); }}");
+        WriteLine($"public ReadOnlySpan<{elementType}> AsReadOnlySpan() {{ fixed(void* ptr = &_0) return new ReadOnlySpan<{elementType}>(ptr, {length}); }}");
+    }
+
+    
+
+    private void WriteVoidPointerType(int length, bool explicitInterface)
+    {
+        WriteLine("#pragma warning disable CS9084");
+
+        if (!explicitInterface)
+            WriteLine($"void* " + string.Join(",", Enumerable.Range(0, length).Select(i => $"_{i}")) + ";");
+        WriteLine();
+        string accessModifier = explicitInterface ? "" : "public";
+        string interfacePrefix = explicitInterface ? "IFixedPtrArray." : "";
+        WriteLine($"{accessModifier} void* {interfacePrefix}this[int i] ");
+        using (BeginBlock())
+        {
+            WriteLine($"readonly get {{ CheckIndex(i); fixed(void* ptr = &_0) return *((void**)ptr + i); }}");
+            WriteLine($"set {{ CheckIndex(i); fixed(void* ptr = &_0) *((void**)ptr + i) = value; }}");
+        }
+
+        WriteLine($"{accessModifier} void* {interfacePrefix}this[uint i] ");
+        using (BeginBlock())
+        {
+            WriteLine($"readonly get {{ CheckIndex(i); fixed(void* ptr = &_0) return *((void**)ptr + i); }}");
+            WriteLine($"set {{ CheckIndex(i); fixed(void* ptr = &_0) *((void**)ptr + i) = value; }}");
+        }
+
+        WriteLine($"{accessModifier} readonly void*[] {interfacePrefix}ToArray()");
+        using (BeginBlock())
+        {
+            WriteLine($"var a = new void*[{length}];");
+            WriteLine($"for (int i = 0; i < {length}; i++) a[i] = this[i];");
+            WriteLine($"return a;");
+        }
+
+        WriteLine($"{accessModifier} void {interfacePrefix}UpdateFrom(void*[] array)");
+        using (BeginBlock())
+        {
+            WriteLine($"int length = Math.Min(array.Length, Length);");
+            if (!explicitInterface)
+                WriteLine($"for(int i = 0; i < length; i++) this[i] = array[i];");
+            else
+                WriteLine($"for(int i = 0; i < length; i++) ((IFixedPtrArray)this)[i] = array[i];");
+
+        }
+        WriteLine("[EditorBrowsable(EditorBrowsableState.Never)]");
+        if (!explicitInterface)
+            WriteLine($"public ref void* GetPinnableReference() => ref _0;");
+        else
+            WriteLine($"ref void* IFixedPtrArray.GetPinnableReference() => throw new InvalidOperationException(\"This method should not be called with boxed type.\");");
+
+        WriteLine($"public Span<IntPtr> AsSpan() {{ fixed(void* ptr = &_0) return new Span<IntPtr>(ptr, {length}); }}");
+        WriteLine($"public ReadOnlySpan<IntPtr> AsReadOnlySpan() {{ fixed(void* ptr = &_0) return new ReadOnlySpan<IntPtr>(ptr, {length}); }}");
+
+        WriteLine("#pragma warning restore CS9084");
+    }
+
+    private void WritePointerType(string elementType, int length)
+    {
+        WriteVoidPointerType(length, true);
+        WriteLine("#pragma warning disable CS9084");
+        WriteLine($"{elementType} " + string.Join(",", Enumerable.Range(0, length).Select(i => $"_{i}")) + ";");
+        WriteLine();
+        WriteLine($"public {elementType} this[int i] ");
+        using (BeginBlock())
+        {
+            WriteLine($"readonly get {{ CheckIndex(i); fixed({elementType}* ptr = &_0) return *(ptr + i); }}");
+            WriteLine($"set {{ CheckIndex(i); fixed({elementType}* ptr = &_0) *(ptr + i) = value; }}");
+        }
+        WriteLine($"public {elementType} this[uint i] ");
+        using (BeginBlock())
+        {
+            WriteLine($"readonly get {{ CheckIndex(i); fixed({elementType}* ptr = &_0) return *(ptr + i); }}");
+            WriteLine($"set {{ CheckIndex(i); fixed({elementType}* ptr = &_0) *(ptr + i) = value; }}");
+        }
+        WriteLine($"public readonly {elementType}[] ToArray()");
+        using (BeginBlock())
+        {
+            WriteLine($"var a = new {elementType}[{length}];");
+            WriteLine($"for (int i = 0; i < {length}; i++) a[i] = this[i];");
+            WriteLine($"return a;");
+        }
+        WriteLine($"public void UpdateFrom({elementType}[] array)");
+        using (BeginBlock())
+        {
+            WriteLine($"int length = Math.Min(array.Length, Length);");
+            WriteLine($"for(int i = 0; i < length; i++) this[i] = array[i];");
+        }
+        WriteLine($"public ref {elementType} GetPinnableReference() => ref _0;");
+        WriteLine("#pragma warning restore CS9084");
+
     }
 }
